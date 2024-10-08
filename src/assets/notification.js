b252c61bd54d552d7d97969cea9781347f62bfc9
jarir2020 <jarir1114@gmail.com>
2024-10-09 05:12:23 +0600

    Update notification.js

--- src/assets/notification.js
+++ src/assets/notification.js
@@ -1,19 +1,15 @@
+
 function showNotification(type, message) {
     // Create a notification div
-    var notification = document.createElement('div');
-    notification.className = 'notification ' + type; // Add type class
-    notification.innerHTML = message;
+    const notification = document.createElement('div');
+    notification.className = 'notification ' + type; // Assign classes based on type
+    notification.innerText = message; // Set the message
 
     // Append notification to body
     document.body.appendChild(notification);
 
-    // Display the notification
-    $(notification).fadeIn();
-
     // Remove the notification after 3 seconds
-    setTimeout(function () {
-        $(notification).fadeOut(function () {
-            this.remove(); // Remove the element from the DOM
-        });
+    setTimeout(() => {
+        notification.remove();
     }, 3000);
 }
